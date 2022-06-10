a6eb40621b4af3917c9ddf1b0a5f074b8941d3d0
Jaime Andres <jaime.artunduaga@uao.edu.co>
2022-06-09 21:19:37 -0500

    Finishing home and about section

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+/*================= MENU SHOW HIDDEN =================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/*Validate if constant exists*/
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/*Validate if constant exists*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*================= REMOVE MENU MOBILE =================*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    //When we click each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+/*================= ACCORDION SKILLS =================*/
+
+const skillsHeader = document.getElementsByClassName('skills__header');
+const skillsArrow = document.querySelectorAll('.skills__arrow');
+
+for (let i = 0; i < skillsHeader.length; i++) {
+    skillsHeader[i].addEventListener("click", function(){
+        let skillList = this.nextElementSibling;
+        skillList.classList.toggle("skills__open");
+        if(skillList.style.maxHeight){
+            skillList.style.maxHeight = null
+            skillsArrow[i].setAttribute("style", "transform: rotate(0deg);");
+        }else{
+            skillList.style.maxHeight = skillList.scrollHeight + "px";
+            skillsArrow[i].setAttribute("style", "transform: rotate(-180deg);");
+        }
+    })
+}
+
